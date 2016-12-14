79727954c0ef1c93129354e484f9558257ebd5f6
Evan Peuvergne <evanpeuvergne@gmail.com>
2016-12-15 00:03:47 +0100

    Add login screen and init inputs style

---
+++ Pepite-iOS/Input.swift
@@ -0,0 +1,21 @@
+//
+//  Input.swift
+//  Pepite-iOS
+//
+//  Created by evan peuvergne on 15/12/2016.
+//  Copyright © 2016 Evan Peuvergne. All rights reserved.
+//
+
+import UIKit
+
+class Input: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
