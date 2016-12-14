6bbfc042c6f398029792e7f12b4dfab7a88b972c
Evan Peuvergne <evanpeuvergne@gmail.com>
2016-12-14 23:19:35 +0100

    Add contstants file

---
+++ Pepite-iOS/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  Pepite-iOS
+//
+//  Created by evan peuvergne on 14/12/2016.
+//  Copyright © 2016 Evan Peuvergne. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+struct Colors{
+    
+    // Tints
+    static let main = UIColor.init(colorLiteralRed: 255.0/255.0, green: 88/255.0, blue: 25/255.0, alpha: 1.0)
+    static let second = UIColor.init(red: 45.5/255.0, green: 78.8/255.0, blue: 96.5/255.0, alpha: 1.0)
+}
