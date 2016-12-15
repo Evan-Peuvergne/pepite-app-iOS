2822e34fc37af3d073e29a5f10afdc6a127fc157
Evan Peuvergne <evanpeuvergne@gmail.com>
2016-12-15 17:44:20 +0100

    Add alamofire

---
+++ Pepite-iOS/Service.swift
@@ -0,0 +1,20 @@
+//
+//  Service.swift
+//  Pepite-iOS
+//
+//  Created by evan peuvergne on 15/12/2016.
+//  Copyright © 2016 Evan Peuvergne. All rights reserved.
+//
+
+import Foundation
+
+
+class Service {
+    
+    
+    /** Initializer **/
+    
+    
+    
+    
+}
--- Podfile
+++
@@ -1,6 +0,0 @@
-# source 'https://github.com/CocoaPods/Specs.git'
-
-target 'Pepite-iOS' do
-  use_frameworks!
-  pod 'Alamofire'
-end
